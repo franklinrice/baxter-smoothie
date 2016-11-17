3be7466b214cc4feaae17f8fbf2bd3c57983383a
Class Account <ee106a-add@imail.EECS.Berkeley.EDU>
2016-11-17 13:08:38 -0800

    initial commit with lab7 baxter framework

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa16/class/ee106a-add/ros_workspaces/PROJECT/baxter-smoothie/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu:/scratch/shared/baxter_ws/devel/lib:/opt/ros/indigo/lib:/usr/sww/lib"
+export PATH="/opt/ros/indigo/bin:/share/b/grading/bin:/home/ff/ee106a/bin:/home/ff/ee106a/bin/arch/x86_64:/usr/local/bin:/usr/sww/bin:/usr/bin:/bin:/usr/ucb:/usr/sfw/bin:/share/b/grading/bin:/share/b/grading/sbin:/share/b/runas/x86_64:/share/b/bin:/home/aa/projects/scheme/bin"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/scratch/shared/baxter_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/cc/ee106a/fa16/class/ee106a-add/ros_workspaces/PROJECT/baxter-smoothie/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cc/ee106a/fa16/class/ee106a-add/ros_workspaces/PROJECT/baxter-smoothie/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa16/class/ee106a-add/ros_workspaces/PROJECT/baxter-smoothie/src:$ROS_PACKAGE_PATH"
